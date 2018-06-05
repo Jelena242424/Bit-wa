cfc0770f1a5508198e82f9bd2d8f70b260c480b8
BIT Student <classroom@bgit.rs>
2018-06-05 14:57:10 +0200

    [bpf-1] kreiranje skracenog email-a

--- React/bitpeople/src/entities/createUser.js
+++ React/bitpeople/src/entities/createUser.js
@@ -6,6 +6,12 @@ class User {
         this.dob = dob;
         this.photoLarge = photoLarge;
     }
+
+    hideEmail() {
+        const emailArr = this.email.split("@");
+        emailArr[0] = `${this.email.slice(0, 3)} ... ${this.email.slice(-3)}`;
+        return emailArr.join("@");
+    }
 }
 
 export  {User};
