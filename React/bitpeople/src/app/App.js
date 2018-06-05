df768883ac43da16a7c73001ccf6df9a6bb7760b
BIT Student <classroom@bgit.rs>
2018-06-05 16:42:31 +0200

    [bpf-2] prikazivanje liste usera

--- React/bitpeople/src/app/App.js
+++ React/bitpeople/src/app/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Header} from "./partials/Header";
+import { Header } from "./partials/Header";
+import { fetchUsers } from '../services/fetchUsers';
+import { UsersList } from "./users/UsersList"
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Header />
+        <UsersList  />
       </React.Fragment>
     );
   }
--- React/bitpeople/src/entities/createUser.js
+++ React/bitpeople/src/entities/createUser.js
@@ -12,6 +12,11 @@ class User {
         emailArr[0] = `${this.email.slice(0, 3)} ... ${this.email.slice(-3)}`;
         return emailArr.join("@");
     }
+    formatDate() {
+        let myDate = new Date(this.dob);
+        let birthday = `${myDate.getDate()}.${myDate.getMonth()+1}.${myDate.getFullYear()}`;
+        return birthday;
+    }
 }
 
 export  {User};
