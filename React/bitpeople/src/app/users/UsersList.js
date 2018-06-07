240af29c70620e42525cc5ca58be4bac338e14ab
Jelena Ristić Opačić <jelena@orangehilldev.com>
2018-06-07 23:11:02 +0200

    [bpf-12] no users found

--- React/bitpeople/src/app/users/UsersList.js
+++ React/bitpeople/src/app/users/UsersList.js
@@ -1,45 +1,45 @@
 import React, { Component } from "react"
 import { SingleUser } from "./SingleUser"
 import { SinglePhotoUser } from "./SinglePhotoUser"
+import { NoUsers} from "../partials/NoUsers"
 // import { fetchUsers } from "../../services/fetchUsers";
 
 
 export class UsersList extends Component {
     constructor(props) {
         super(props);
-       
+
 
     }
     // ne mora da e pise kostruktor da bi setovali state. vec state = {}. On automatski u pozadini napise to kao ovde sto je u konstruktoru.
 
-   
-   
-    renderSingleUser(users) {
 
+    renderSingleUser(users) {
         if (this.props.inputValue) {
-            return (
-                <div className="collection">
-                    {users.filter((user) => {
-                         console.log("aa", user.name, this.props.inputValue)
-                        return user.name.includes(this.props.inputValue)
-                    })
-                    .map((user, index) => {
-                            return <SingleUser myUser={user} key={index} />
-                        })}
-                </div>
-            )
+            const filteredUsers = users.filter((user) => {
+                return user.name.includes(this.props.inputValue)
+            });
+            if (filteredUsers.length > 0) {
+                return (
+                    <div className="collection">
+                        {filteredUsers
+                            .map((user, index) => {
+                                return <SingleUser myUser={user} key={index} />
+                            })}
+                    </div>
+                )
+            } else {
+                return (<NoUsers />)
+            }
         } else {
             return (
                 <div className="collection">
                     {users.map((user, index) => {
-                            return <SingleUser myUser={user} key={index} />
-                        })}
+                        return <SingleUser myUser={user} key={index} />
+                    })}
                 </div>
             )
-
         }
-
-        
     }
 
     renderSinglePhotoUser(users) {
@@ -47,10 +47,10 @@ export class UsersList extends Component {
             return (
                 <div className="collection">
                     {users.filter((user) => {
-                       
+
                         return user.name.includes(this.props.inputValue)
                     })
-                    .map((user, index) => {
+                        .map((user, index) => {
                             return <SinglePhotoUser myUser={user} key={index} />
                         })}
                 </div>
@@ -59,8 +59,8 @@ export class UsersList extends Component {
             return (
                 <div className="collection">
                     {users.map((user, index) => {
-                            return <SinglePhotoUser myUser={user} key={index} />
-                        })}
+                        return <SinglePhotoUser myUser={user} key={index} />
+                    })}
                 </div>
             )
 
